a82c47b7d221b57a9b66ae418834b1e64bc7244e
ecoshub <emrecanocak@windowslive.com>
2020-07-23 11:05:19 +0300

    unnecassary import statements removed

--- CustomerService/Contexts/MongoSettings.cs
+++ CustomerService/Contexts/MongoSettings.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CustomerService.Model
-{
-    public class MongoSettings
-    {
+﻿namespace CustomerService.Model {
+    public class MongoSettings {
         public string Connection { get; set; }
         public string DatabaseName { get; set; }
     }
--- CustomerService/Contexts/MongoUserDBContext.cs
+++ CustomerService/Contexts/MongoUserDBContext.cs
@@ -1,25 +1,17 @@
 ﻿using Microsoft.Extensions.Options;
 using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace CustomerService.Model
-{
-    public class MongoUserDBContext : IMongoUserDBContext
-    {
+namespace CustomerService.Model {
+    public class MongoUserDBContext : IMongoUserDBContext {
         private IMongoDatabase _db { get; set; }
         private MongoClient _mongoClient { get; set; }
         public IClientSessionHandle Session { get; set; }
-        public MongoUserDBContext(IOptions<MongoSettings> configuration)
-        {
+        public MongoUserDBContext(IOptions<MongoSettings> configuration) {
             _mongoClient = new MongoClient(configuration.Value.Connection);
             _db = _mongoClient.GetDatabase(configuration.Value.DatabaseName);
         }
 
-        public IMongoCollection<T> GetCollection<T>(string name)
-        {
+        public IMongoCollection<T> GetCollection<T>(string name) {
             name = name.ToLower();
             return _db.GetCollection<T>(name);
         }
