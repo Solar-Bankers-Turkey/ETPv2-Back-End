102f330229c31ae7bb10dbfafcf93de14930e2cb
ecoshub <emrecanocak@windowslive.com>
2020-07-24 09:24:16 +0300

    register end point partial imp

--- CustomerService/Controllers/CustomerController.cs
+++ CustomerService/Controllers/CustomerController.cs
@@ -1,33 +1,79 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
+using CustomerService.DataTransferObjects;
 using CustomerService.Models;
 using CustomerService.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace CustomerService.Controllers {
+
     [Route("customers")]
     [ApiController]
     public class CustomerController : ControllerBase {
+
         private readonly IUserRepository _customerRepository;
-        // public Crypto encoder { get; set; }
-        // public string DefaultLanguage { get; set; }
-        public CustomerController(IUserRepository customerRepository) {
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _config;
+
+        public CustomerController(IUserRepository customerRepository, IMapper mapper, IConfiguration config) {
             _customerRepository = customerRepository;
-            // encoder = new Crypto();
-            // DefaultLanguage = "TR";
+            _mapper = mapper;
+            _config = config;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<ActionResult<User>> registerFirstStep([FromBody] RegisterFirstStep registerFirst) {
+            // ! check already exists later
+            // ! check null object
+            var salt = _config.GetValue<string>("salt");
+            string passwordHash = salt + BCrypt.Net.BCrypt.HashPassword(registerFirst.password);
+            User user = _mapper.Map<User>(registerFirst);
+            // ! generate persons wallet
+            user.passwordHash = passwordHash;
+            try {
+                _customerRepository.Create(user);
+            } catch {
+                // ! change status code with decent status code
+                return StatusCode(304);
+            }
+            // ! create decent user link
+            // ! send mail for next step
+            return Created("", user);
         }
 
         [HttpGet]
         [Route("get")]
-        public async Task<ActionResult<IEnumerable<User>>> GetAllCustomers() {
-            var customers = await _customerRepository.Get();
-            return Ok(customers);
+        public async Task<ActionResult<User>> get() {
+            var query = HttpContext.Request.Query;
+            if (query.Count == 0) {
+                var customers = await _customerRepository.Get();
+                return Ok(customers);
+            }
+            var result = _customerRepository.Query(query);
+            return Ok(result.Result);
+        }
+
+        [HttpGet]
+        [Route("exists")]
+        public async Task<ActionResult<bool>> exist() {
+            var query = HttpContext.Request.Query;
+            if (query.Count == 0) {
+                return Ok(false);
+            }
+            var result = _customerRepository.Query(query);
+            return Ok(result.Result != null);
         }
 
         [HttpGet]
         [Route("index")]
         public async Task<ActionResult> index() {
-            return Ok();
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword("emre");
+            bool verified = BCrypt.Net.BCrypt.Verify("emre", passwordHash);
+            return Ok(verified);
         }
     }
 }
