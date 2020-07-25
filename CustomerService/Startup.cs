bb1578cafff9e7accbfc0535555527164db9796b
ecoshub <emrecanocak@windowslive.com>
2020-07-25 21:55:21 +0300

    swagger imp. initial commit

--- CustomerService/Startup.cs
+++ CustomerService/Startup.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Reflection;
 using AutoMapper;
 using CustomerService.Contexts;
 using CustomerService.Email;
@@ -8,6 +10,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace CustomerService {
     public class Startup {
@@ -28,10 +31,28 @@ namespace CustomerService {
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IEmailSender, EmailSender>();
             services.AddControllers();
+            // swagger for api gen.
+            // services.AddSwaggerGen();
+            services.AddSwaggerGen(swagger => {
+                swagger.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "ETPv2 CustomerService",
+                        Description = "ETP detailed customer service API"
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                swagger.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            // api gen middleware
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API");
+            });
+
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
