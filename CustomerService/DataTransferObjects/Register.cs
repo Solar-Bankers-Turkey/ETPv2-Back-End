5aacc880472e0cdb60994e0d5f43c18929e4fe1a
ecoshub <emrecanocak@windowslive.com>
2020-07-25 21:54:56 +0300

    register model 'required' field added

--- CustomerService/DataTransferObjects/Register.cs
+++ CustomerService/DataTransferObjects/Register.cs
@@ -2,16 +2,19 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace CustomerService.DataTransferObjects {
     public class Register {
-
+        [BsonRequired]
         [BsonElement("name")]
         public string name { get; set; }
 
+        [BsonRequired]
         [BsonElement("lastname")]
         public string lastname { get; set; }
 
+        [BsonRequired]
         [BsonElement("email")]
         public string email { get; set; }
 
+        [BsonRequired]
         [BsonElement("password")]
         public string password { get; set; }
     }
