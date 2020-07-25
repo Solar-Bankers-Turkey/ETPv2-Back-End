9105183e584a16202a9457bc8a44914567d0d947
ecoshub <emrecanocak@windowslive.com>
2020-07-25 17:16:26 +0300

    User->Register and Verify->Detail dto mapping profiles

--- CustomerService/Profiles/UserProfile.cs
+++ CustomerService/Profiles/UserProfile.cs
@@ -3,11 +3,14 @@ using CustomerService.DataTransferObjects;
 using CustomerService.Models;
 
 namespace CustomerService.Profiles {
+
     public class UserProfile : Profile {
 
         public UserProfile() {
-            CreateMap<User, RegisterFirstStep>();
-            CreateMap<RegisterFirstStep, User>();
+            CreateMap<User, Register>();
+            CreateMap<Register, User>();
+            CreateMap<Verify, Detail>();
+            CreateMap<Detail, Verify>();
         }
     }
 }
